f9de358462d68cb367b96f97314b173b5958a204
De1oNn <nasanjargal20040524@gmail.com>
2024-12-02 12:00:53 +0800

    first

---
+++ index.js
@@ -0,0 +1,128 @@
+//alert("hello world");
+console.log('hello world');
+
+//let a = 3, b = 4, c = 5;
+//console.log(a + b + c);
+//2
+//let a = 12;
+//let b = 23;
+
+//console.log(a + b);
+
+//3
+//let a = 5;
+
+//console.log(Math.pow(a, 3));
+
+//4
+
+//let a = 4, b = 6;
+//console.log(a * b, (a + b)*2);
+
+//5
+
+//let x = 1;
+//y = 3*x - 5;
+
+//console.log(y)
+
+//6
+
+//let x = -2
+
+//y = (Math.pow(2, (4*x)))- 3*x + 5;
+//console.log(y)
+//y = Math.pow((x), 2)
+//w = y * 4
+//console.log(w)
+//console.log(y)
+//q = w - 3*x + 5
+//console.log(q)
+
+
+//7
+
+//let asd = 476;
+
+//let suuliintoo = asd % 10
+//console.log(suuliintoo)
+//8
+//let b = 423;
+//let g = Math.floor(b % 100 /10)
+//console.log(g)
+
+//9
+//let c = 123;
+
+//let suuliintoo = c % 10 
+//console.log(suuliintoo)
+
+//let h = c / 100
+//console.log(Math.floor(h))
+
+//let l = c % 100
+//let p = l / 10 
+//console.log(Math.floor(p))
+
+//let all = (suuliintoo * Math.floor(h) * Math.floor(p))
+//console.log(Math.floor(all))
+//10
+//let int =  200
+
+//f = Math.floor(int / 60);
+//m = ((int % 60));
+// console.log(m)
+// console.log(f)
+
+//11
+
+//let all = 3612;
+
+//let second = all % 3600
+//let hour = Math.floor(all / 3600)
+//let minute = Math.floor((all % 3600) / 60 )
+//console.log(hour, minute, second)
+
+// 12
+//let a = 3
+//let b = 4
+
+//let c = 3 * 60
+//let d = c + b 
+//console.log(d) 
+
+// 13
+//let a = 44 
+
+//let honog = Math.floor(44 / 24)
+//let tsag = a % 24
+//console.log(honog, tsag)
+
+ // 14 
+//let a = 27
+
+//let sar = a % 12
+//let jil = Math.floor(a / 12)
+//console.log(jil, sar)
+
+// 15
+
+//let a = 1
+//let b = 2
+//let c = 3
+
+//let d = Math.max(a, b, c)
+//console.log(d)
+
+// 16
+//let a = 9 
+//let b = 6
+//let c = Math.min(a, b)
+//console.log(c)
+
+// 17 
+let a = 85
+let b = 75
+let c = 96
+let d = 69
+
