564da80e04d09383df0cf3dafd10f94f20a3abc3
De1oNn <nasanjargal20040524@gmail.com>
2024-12-03 12:32:05 +0800

    eheh

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ console.log('hello world');
 //let tsag = a % 24
 //console.log(honog, tsag)
 
- // 14 
+// 14 
 //let a = 27
 
 //let sar = a % 12
@@ -124,7 +124,7 @@ console.log('hello world');
 //let a = 10
 //let b = 3 
 //let c = 5
- // 18
+// 18
 
 // 18 
 //let a = 11
@@ -144,8 +144,337 @@ console.log('hello world');
 //console.log(d)
 
 // 20
- let a = 3
+// let a = 3
 
- if (b = a * 2){
-    console.log(b)
- }
+// if (b = a * 2){
+//    console.log(b)
+// }
+
+// 7103 
+
+//let a = 1
+//let b = 2
+//let c = 3
+
+//console.log(Math.max(a, b, c))
+
+// 7104 
+
+//let a = 1
+//let b = 2
+//let c = 3
+//let d = 4
+
+//console.log(Math.min(a, b, c, d))
+
+// 7105
+
+//let a = 85
+//let b = 90
+//let c = 70
+//let d = 69
+
+//let sum = 0 ;
+//console.log(sum);
+
+//if (a>80){
+//   sum = sum+a
+//}
+//console.log(sum);
+//if(b>80){
+//   sum = sum+b
+//}
+//console.log(sum);
+//if(c>80){
+//   sum = sum+c
+//}
+//console.log(sum);
+//if(d>80){
+//   sum = sum+d
+//}
+//console.log(sum);
+
+// test - hicheel
+
+//let age = 5;
+
+//if (age >= 20){
+//   console.log('tanii nas', age);
+//   console.log('ta nasand hursen baina');
+//} else {
+//   console.log('ta nasand hureegui baina');
+//   console.log('tanii nas', age);
+//}
+
+//let a = 80;
+//let b = 1000;
+//let c = 10;
+
+//if (a>b){
+//   if(a>c) {
+//      console.log(a, "too ih")
+//   }
+//} if (b > a) {
+//   if (b > c ) {
+//      console.log(b, "ih too")
+//   }
+//} if (c > a) {
+//   if (c > b) {
+//      console.log(c, "ih too")
+//   }
+//}
+
+
+// 7106 
+
+//let a = 3
+//let b = 6
+//let c = 2 
+//let d = 4
+
+//let sum = 1
+
+//if (a>5) {
+//   sum = sum * a
+//}
+//if (b>5) {
+//   sum = sum * b
+//}
+//if (c>5) {
+//   sum = sum * c
+//}
+//if (d>5) {
+//   sum = sum * d
+//}
+
+//console.log(sum);
+
+// 7107 
+
+//let a = 10
+//let b = 10 
+//let c = 1000
+
+//let sum = 0;
+
+//if (a % 2 == 0) {
+//   sum = sum + a
+//}
+//if (b % 2 == 0) {
+//   sum = sum + b
+//}
+//if (c % 2 == 0) {
+//   sum = sum + c
+//} 
+//console.log(sum);
+
+// 7108
+
+//let a = 5 
+//let b = 3
+//let c = 3
+
+//let sum = 1
+
+//if (a % 2 == 0) {
+//}  else {
+//   sum = sum * a
+//}
+//if (b % 2 == 0) {
+//} else {
+//   sum = sum * b
+//}
+//if (c % 2 == 0) {
+//} else {
+//   sum = sum * c
+//}
+//console.log(sum);
+
+// 7109 
+
+//let a = 1 
+//let b = 5
+//let c = 5
+
+//let sum = 0;
+
+//if (5 == 1 * a) {
+//   sum = sum + a
+//}
+//if (5 == 1 * b) {
+//   sum = sum + b
+//}
+//if (5 == 1 * c) {
+//   sum = sum + c
+//}
+//console.log(sum);
+
+// 7110
+
+//let a = 3
+//let b = 12
+//let c = 9 
+//let d = 9
+
+//let sum = 1;
+
+//if (a % 2 == 0) {
+//} else {
+//   sum = sum + 1
+//}
+//if (b % 2 == 0) {
+//} else {
+//   sum = sum + 1
+//}
+//if (c % 2 == 0) {
+//} else {
+//   sum = sum + 1
+//}
+//if (d % 2 == 0) {
+//} else {
+//   sum = sum + 1
+//}
+//console.log(sum);
+
+// 7111
+
+//let a = 7
+//let b = 22
+//let c = 13 
+//let d = 30
+
+//let sum = 0;
+
+//if (a % 11 == 0){
+//} else {
+//   sum = sum + a
+//}
+//if (b % 11 == 0){
+//} else {
+//   sum = sum + b
+//}
+//if (c % 11 == 0){
+//} else {
+//   sum = sum + c
+//}
+//if (d % 11 == 0){
+//} else {
+//   sum = sum + d
+//}
+//console.log(sum);
+
+// 7112 
+
+//let a = 11
+
+//if(a > 10) {
+//   console.log("Yes")
+//} else {
+//   console.log("No"); 
+//}
+
+// 7113
+
+//let a = 4
+
+//if(a < 5){
+//   console.log("Yes")
+//}  else {
+//   console.log("No")
+//}
+
+// 7114
+
+//let a = 3
+//let b = 4
+//let c = 5
+
+//if(a % 2 == 0){
+//   console.log("Yes");
+//} else {
+//   console.log("No")
+//}
+//if(b % 2 == 0){
+//   console.log("Yes");
+//} else {
+//   console.log("No")
+//}
+//if(c % 2 == 0) {
+//   console.log("Yes")
+//} else {
+//   console.log("No")
+//}
+
+// teams1
+
+//let a = 2
+//let b = 1
+
+//if(a % 2 == 0){
+//   console.log("tegsh")
+//} else {
+//   console.log("sondgoi")
+//}
+//if (b % 2 == 0){
+//   console.log("tegsh");
+//} else {
+//   console.log("sondgoi")
+//}
+
+// teams2
+
+//let a = -1
+
+//if (a < 0){
+//   console.log("Negative")
+//} else if (a >= 1){
+//   console.log("Positive")
+//} else {
+//   console.log("Zero")
+//}
+
+// teams3
+
+//let a = 6
+//let b = 3
+
+//if(a % b == 0){
+//   console.log("Huvaagddag")
+//} else {
+//   console.log("huvaagdadgui")
+//}
+
+// teams4
+
+//let a = 69;
+
+//if (a >= 90) {
+//   console.log("A");
+//} else if (90 > a && a >= 80) {
+//   console.log("B");
+//} else if (80 > a && a >= 70) {
+//   console.log("C");
+//} else if (70 > a && a >= 60) {
+//   console.log("D");
+//} else {
+//   console.log("F");
+//}
+
+// teams5
+
+//let a = 100
+
+//let sum;
+//if(sum=(a * 9 / 5) + 32) {
+//   console.log(sum)
+//}
+
+// teams6
+
+//4 t hugaavddag 400 d huvaagdadgui
+
+// teams7
+
+let a = 1
+
+if(a )
